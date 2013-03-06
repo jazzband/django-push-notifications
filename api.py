3592b601caaad58e81695df42f6f93cc1d940c71
Jerome Leclanche <adys.wh@gmail.com>
2013-03-06 11:06:50 +0000

    Add simple AuthenticatedResources for the api

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 from tastypie.authorization import Authorization
+from tastypie.authentication import BasicAuthentication
+from tastypie.fields import ForeignKey
 from tastypie.resources import ModelResource
 from .models import APNSDevice, GCMDevice
 
@@ -14,3 +16,27 @@ class GCMDeviceResource(ModelResource):
 		authorization = Authorization()
 		queryset = GCMDevice.objects.all()
 		resource_name = "device/gcm"
+
+
+class APNSDeviceAuthenticatedResource(APNSDeviceResource):
+	# user = ForeignKey(UserResource, "user")
+
+	class Meta(APNSDeviceResource.Meta):
+		authentication = BasicAuthentication()
+		# authorization = SameUserAuthorization()
+
+	def obj_create(self, bundle, **kwargs):
+		bundle.data["user_id"] = bundle.request.user.id
+		return super(APNSDeviceAuthenticatedResource, self).obj_create(bundle, **kwargs)
+
+
+class GCMDeviceAuthenticatedResource(GCMDeviceResource):
+	# user = ForeignKey(UserResource, "user")
+
+	class Meta(GCMDeviceResource.Meta):
+		authentication = BasicAuthentication()
+		# authorization = SameUserAuthorization()
+
+	def obj_create(self, bundle, **kwargs):
+		bundle.data["user_id"] = bundle.request.user.id
+		return super(GCMDeviceAuthenticatedResource, self).obj_create(bundle, **kwargs)
