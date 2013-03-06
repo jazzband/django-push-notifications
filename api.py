e767fc2dc3ae08244bd76c5ee2eb1ce38dc010d8
Jerome Leclanche <adys.wh@gmail.com>
2013-03-06 08:22:33 +0000

    Add an api resource for APNSDevices

--- api.py
+++ api.py
@@ -1,6 +1,13 @@
 from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
-from .models import GCMDevice
+from .models import APNSDevice, GCMDevice
+
+
+class APNSDeviceResource(ModelResource):
+	class Meta:
+		authorization = Authorization()
+		queryset = APNSDevice.objects.all()
+		resource_name = "apnsdevice"
 
 class GCMDeviceResource(ModelResource):
 	class Meta:
