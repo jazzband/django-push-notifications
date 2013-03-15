95f1bf13b61d69e3892a1afeb05d7141e51f825c
Jerome Leclanche <adys.wh@gmail.com>
2013-03-15 00:54:29 +0000

    Release v0.8 alpha

---
+++ README.rst
@@ -0,0 +1,4 @@
+django-push-notifications
+=========================
+
+A minimal Django app that implements Device models that can send messages through APNS and GCM.
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os.path
+from distutils.core import setup
+
+README = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
+
+CLASSIFIERS = [
+	"Development Status :: 3 - Alpha",
+	"Environment :: Web Environment",
+	"Framework :: Django",
+	"Intended Audience :: Developers",
+	"License :: OSI Approved :: MIT License",
+	"Programming Language :: Python",
+	"Topic :: Software Development :: Libraries :: Python Modules",
+	"Topic :: System :: Networking",
+]
+
+VERSION = "0.8"
+
+setup(
+	name = "django-push-notifications",
+	py_modules = ["push_notifications"],
+	package_dir = {"push_notifications": "push_notifications"},
+	author = "Jerome Leclanche",
+	author_email = "jerome.leclanche+pypi@gmail.com",
+	classifiers = CLASSIFIERS,
+	description = "Send push notifications to mobile devices through GCM or APNS in Django.",
+	download_url = "https://github.com/Adys/django-push-notifications/tarball/master",
+	long_description = README,
+	url = "https://github.com/Adys/django-push-notifications",
+	version = VERSION,
+)
