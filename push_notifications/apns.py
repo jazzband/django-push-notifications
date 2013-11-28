31235c4f59d9e3ca01f7cfa50eb51a6ae0f4b8e2
Florian Finke <flo@randomknowledge.org>
2013-11-28 15:17:10 +0100

    pep8

--- push_notifications/apns.py
+++ push_notifications/apns.py
@@ -13,105 +13,112 @@ from . import NotificationError, PUSH_NOTIFICATIONS_SETTINGS as SETTINGS
 
 
 class APNSError(NotificationError):
-	pass
+    pass
+
 
 class APNSDataOverflow(APNSError):
-	pass
+    pass
+
 
 SETTINGS.setdefault("APNS_PORT", 2195)
 if settings.DEBUG:
-	SETTINGS.setdefault("APNS_HOST", "gateway.sandbox.push.apple.com")
+    SETTINGS.setdefault("APNS_HOST", "gateway.sandbox.push.apple.com")
 else:
-	SETTINGS.setdefault("APNS_HOST", "gateway.push.apple.com")
+    SETTINGS.setdefault("APNS_HOST", "gateway.push.apple.com")
 
 APNS_MAX_NOTIFICATION_SIZE = 256
 
 
 def _apns_create_socket():
-	import ssl
-	from socket import socket
-	from django.core.exceptions import ImproperlyConfigured
+    import ssl
+    from socket import socket
+    from django.core.exceptions import ImproperlyConfigured
 
-	sock = socket()
-	certfile = SETTINGS.get("APNS_CERTIFICATE")
-	if not certfile:
-		raise ImproperlyConfigured('You need to set PUSH_NOTIFICATIONS_SETTINGS["APNS_CERTIFICATE"] to send messages through APNS.')
+    sock = socket()
+    certfile = SETTINGS.get("APNS_CERTIFICATE")
+    if not certfile:
+        raise ImproperlyConfigured(
+            'You need to set PUSH_NOTIFICATIONS_SETTINGS["APNS_CERTIFICATE"] to send messages through APNS.')
 
-	try:
-		f = open(certfile, "r")
-		f.read()
-		f.close()
-	except Exception, e:
-		raise ImproperlyConfigured("The APNS certificate file at %r is not readable: %s" % (certfile, e))
+    try:
+        f = open(certfile, "r")
+        f.read()
+        f.close()
+    except Exception, e:
+        raise ImproperlyConfigured("The APNS certificate file at %r is not readable: %s" % (certfile, e))
 
-	sock = ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_SSLv3, certfile=certfile)
-	sock.connect((SETTINGS["APNS_HOST"], SETTINGS["APNS_PORT"]))
+    sock = ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_SSLv3, certfile=certfile)
+    sock.connect((SETTINGS["APNS_HOST"], SETTINGS["APNS_PORT"]))
 
-	return sock
+    return sock
+
 
 def _apns_pack_message(token, data):
-	format = "!cH32sH%ds" % (len(data))
-	return struct.pack(format, b"\0", 32, unhexlify(token), len(data), data)
+    format = "!cH32sH%ds" % (len(data))
+    return struct.pack(format, b"\0", 32, unhexlify(token), len(data), data)
 
-def _apns_send(token, alert, badge=0, sound="chime", content_available=False, action_loc_key=None, loc_key=None, loc_args=[], extra={}, socket=None):
-	data = {}
 
-	if action_loc_key or loc_key or loc_args:
-		alert = {"body": alert}
-		if action_loc_key:
-			alert["action-loc-key"] = action_loc_key
-		if loc_key:
-			alert["loc-key"] = loc_key
-		if loc_args:
-			alert["loc-args"] = loc_args
+def _apns_send(token, alert, badge=0, sound="chime", content_available=False, action_loc_key=None, loc_key=None,
+               loc_args=[], extra={}, socket=None):
+    data = {}
 
-	data["alert"] = alert
+    if action_loc_key or loc_key or loc_args:
+        alert = {"body": alert}
+        if action_loc_key:
+            alert["action-loc-key"] = action_loc_key
+        if loc_key:
+            alert["loc-key"] = loc_key
+        if loc_args:
+            alert["loc-args"] = loc_args
 
-	if badge:
-		data["badge"] = badge
+    data["alert"] = alert
 
-	if sound:
-		data["sound"] = sound
+    if badge:
+        data["badge"] = badge
 
-	if content_available:
-		data["content-available"] = 1
+    if sound:
+        data["sound"] = sound
 
-	data.update(extra)
+    if content_available:
+        data["content-available"] = 1
 
-	# convert to json, avoiding unnecessary whitespace with separators
-	data = json.dumps({"aps": data}, separators=(",", ":"))
+    data.update(extra)
 
-	if len(data) > APNS_MAX_NOTIFICATION_SIZE:
-		raise APNSDataOverflow("Notification body cannot exceed %i bytes" % (APNS_MAX_NOTIFICATION_SIZE))
+    # convert to json, avoiding unnecessary whitespace with separators
+    data = json.dumps({"aps": data}, separators=(",", ":"))
 
-	data = _apns_pack_message(token, data)
+    if len(data) > APNS_MAX_NOTIFICATION_SIZE:
+        raise APNSDataOverflow("Notification body cannot exceed %i bytes" % (APNS_MAX_NOTIFICATION_SIZE))
 
-	if socket:
-		socket.write(data)
-	else:
-		socket = _apns_create_socket()
-		socket.write(data)
-		socket.close()
+    data = _apns_pack_message(token, data)
+
+    if socket:
+        socket.write(data)
+    else:
+        socket = _apns_create_socket()
+        socket.write(data)
+        socket.close()
 
 
 def apns_send_message(registration_id, data, **kwargs):
-	"""
-	Sends an APNS notification to a single registration_id.
-	This will send the notification as form data.
-	If sending multiple notifications, it is more efficient to use
-	apns_send_bulk_message()
-	Note that \a data should always be a string.
-	"""
+    """
+    Sends an APNS notification to a single registration_id.
+    This will send the notification as form data.
+    If sending multiple notifications, it is more efficient to use
+    apns_send_bulk_message()
+    Note that \a data should always be a string.
+    """
 
-	return _apns_send(registration_id, data, **kwargs)
+    return _apns_send(registration_id, data, **kwargs)
+
 
 def apns_send_bulk_message(registration_ids, data, **kwargs):
-	"""
-	Sends an APNS notification to one or more registration_ids.
-	The registration_ids argument needs to be a list.
-	"""
-	socket = _apns_create_socket()
-	for registration_id in registration_ids:
-		_apns_send(registration_id, data, socket=socket, **kwargs)
+    """
+    Sends an APNS notification to one or more registration_ids.
+    The registration_ids argument needs to be a list.
+    """
+    socket = _apns_create_socket()
+    for registration_id in registration_ids:
+        _apns_send(registration_id, data, socket=socket, **kwargs)
 
-	socket.close()
+    socket.close()
--- setup.py
+++ setup.py
@@ -6,27 +6,27 @@ from distutils.core import setup
 README = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
 
 CLASSIFIERS = [
-	"Development Status :: 3 - Alpha",
-	"Environment :: Web Environment",
-	"Framework :: Django",
-	"Intended Audience :: Developers",
-	"License :: OSI Approved :: MIT License",
-	"Programming Language :: Python",
-	"Topic :: Software Development :: Libraries :: Python Modules",
-	"Topic :: System :: Networking",
+    "Development Status :: 3 - Alpha",
+    "Environment :: Web Environment",
+    "Framework :: Django",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: System :: Networking",
 ]
 
 VERSION = "0.8"
 
 setup(
-	name = "django-push-notifications",
-	packages = ["push_notifications"],
-	author = "Jerome Leclanche",
-	author_email = "jerome.leclanche+pypi@gmail.com",
-	classifiers = CLASSIFIERS,
-	description = "Send push notifications to mobile devices through GCM or APNS in Django.",
-	download_url = "https://github.com/Adys/django-push-notifications/tarball/master",
-	long_description = README,
-	url = "https://github.com/Adys/django-push-notifications",
-	version = VERSION,
+    name="django-push-notifications",
+    packages=["push_notifications"],
+    author="Jerome Leclanche",
+    author_email="jerome.leclanche+pypi@gmail.com",
+    classifiers=CLASSIFIERS,
+    description="Send push notifications to mobile devices through GCM or APNS in Django.",
+    download_url="https://github.com/Adys/django-push-notifications/tarball/master",
+    long_description=README,
+    url="https://github.com/Adys/django-push-notifications",
+    version=VERSION,
 )
