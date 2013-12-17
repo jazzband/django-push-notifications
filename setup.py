8c49ecc5f8cce6eaa0b1cb6b62b900f0fee7dc96
Jerome Leclanche <adys.wh@gmail.com>
2013-12-17 17:18:18 +0000

    Add python version classifiers to setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,10 @@ CLASSIFIERS = [
 	"Intended Audience :: Developers",
 	"License :: OSI Approved :: MIT License",
 	"Programming Language :: Python",
+	"Programming Language :: Python :: 2.6",
+	"Programming Language :: Python :: 2.7",
+	"Programming Language :: Python :: 3",
+	"Programming Language :: Python :: 3.3",
 	"Topic :: Software Development :: Libraries :: Python Modules",
 	"Topic :: System :: Networking",
 ]
