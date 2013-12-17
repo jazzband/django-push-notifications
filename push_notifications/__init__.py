5ebd80241adae6ccc1acc345ca7c95d7c1d5dee9
Jerome Leclanche <adys.wh@gmail.com>
2013-12-17 17:19:34 +0000

    Release version 0.9. Upgrade to Beta.

--- push_notifications/__init__.py
+++ push_notifications/__init__.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 __author__ = "Jerome Leclanche"
 __email__ = "jerome.leclanche+pypi@gmail.com"
-__version__ = "0.8"
+__version__ = "0.9"
 
 
 PUSH_NOTIFICATIONS_SETTINGS = getattr(settings, "PUSH_NOTIFICATIONS_SETTINGS", {})
