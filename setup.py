84da867c380812b596b7087d9342b271801de808
Jerome Leclanche <jerome@leclan.ch>
2014-05-12 15:00:21 +0100

    Add 3.4 pypi classifier

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ CLASSIFIERS = [
 	"Programming Language :: Python :: 2.7",
 	"Programming Language :: Python :: 3",
 	"Programming Language :: Python :: 3.3",
+	"Programming Language :: Python :: 3.4",
 	"Topic :: Software Development :: Libraries :: Python Modules",
 	"Topic :: System :: Networking",
 ]
