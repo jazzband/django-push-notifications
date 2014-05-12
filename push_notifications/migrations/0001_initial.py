fc55752d5e333496624dff1f9fd9425258511082
Jerome Leclanche <jerome@leclan.ch>
2014-05-12 15:08:44 +0100

    Avoid mixing spaces and tabs

--- push_notifications/migrations/0001_initial.py
+++ push_notifications/migrations/0001_initial.py
@@ -20,7 +20,7 @@ class Migration(SchemaMigration):
 	def forwards(self, orm):
 		# Adding model "GCMDevice"
 		db.create_table(u"push_notifications_gcmdevice", (
-			(u"id", self.gf("django.db.models.fields.AutoField")(primary_key=True)),
+			("id", self.gf("django.db.models.fields.AutoField")(primary_key=True)),
 			("name", self.gf("django.db.models.fields.CharField")(max_length=255, null=True, blank=True)),
 			("active", self.gf("django.db.models.fields.BooleanField")(default=True)),
 			("user", self.gf("django.db.models.fields.related.ForeignKey")(
@@ -32,7 +32,7 @@ class Migration(SchemaMigration):
 
 		# Adding model "APNSDevice"
 		db.create_table(u"push_notifications_apnsdevice", (
-			(u"id", self.gf("django.db.models.fields.AutoField")(primary_key=True)),
+			("id", self.gf("django.db.models.fields.AutoField")(primary_key=True)),
 			("name", self.gf("django.db.models.fields.CharField")(max_length=255, null=True, blank=True)),
 			("active", self.gf("django.db.models.fields.BooleanField")(default=True)),
 			("user", self.gf("django.db.models.fields.related.ForeignKey")(
@@ -52,17 +52,17 @@ class Migration(SchemaMigration):
 	models = {
 		u"auth.group": {
 			"Meta": {"object_name": "Group"},
-			u"id": ("django.db.models.fields.AutoField", [], {"primary_key": "True"}),
+			"id": ("django.db.models.fields.AutoField", [], {"primary_key": "True"}),
 			"name": ("django.db.models.fields.CharField", [], {"unique": "True", "max_length": "80"}),
 			"permissions": ("django.db.models.fields.related.ManyToManyField", [],
-							{"to": u"orm['auth.Permission']", "symmetrical": "False", "blank": "True"})
+				{"to": u"orm['auth.Permission']", "symmetrical": "False", "blank": "True"})
 		},
 		u"auth.permission": {
 			"Meta": {"ordering": "(u'content_type__app_label', u'content_type__model', u'codename')",
-					 "unique_together": "((u'content_type', u'codename'),)", "object_name": "Permission"},
+				"unique_together": "((u'content_type', u'codename'),)", "object_name": "Permission"},
 			"codename": ("django.db.models.fields.CharField", [], {"max_length": "100"}),
 			"content_type": ("django.db.models.fields.related.ForeignKey", [], {"to": u"orm['contenttypes.ContentType']"}),
-			u"id": ("django.db.models.fields.AutoField", [], {"primary_key": "True"}),
+			"id": ("django.db.models.fields.AutoField", [], {"primary_key": "True"}),
 			"name": ("django.db.models.fields.CharField", [], {"max_length": "50"})
 		},
 		"%s.%s" % (User._meta.app_label, User._meta.module_name): {
@@ -71,8 +71,8 @@ class Migration(SchemaMigration):
 			"email": ("django.db.models.fields.EmailField", [], {"max_length": "75", "blank": "True"}),
 			"first_name": ("django.db.models.fields.CharField", [], {"max_length": "30", "blank": "True"}),
 			"groups": ("django.db.models.fields.related.ManyToManyField", [],
-					   {"to": u"orm['auth.Group']", "symmetrical": "False", "blank": "True"}),
-			u"id": ("django.db.models.fields.AutoField", [], {"primary_key": "True"}),
+				{"to": u"orm['auth.Group']", "symmetrical": "False", "blank": "True"}),
+			"id": ("django.db.models.fields.AutoField", [], {"primary_key": "True"}),
 			"is_active": ("django.db.models.fields.BooleanField", [], {"default": "True"}),
 			"is_staff": ("django.db.models.fields.BooleanField", [], {"default": "False"}),
 			"is_superuser": ("django.db.models.fields.BooleanField", [], {"default": "False"}),
@@ -80,14 +80,14 @@ class Migration(SchemaMigration):
 			"last_name": ("django.db.models.fields.CharField", [], {"max_length": "30", "blank": "True"}),
 			"password": ("django.db.models.fields.CharField", [], {"max_length": "128"}),
 			"user_permissions": ("django.db.models.fields.related.ManyToManyField", [],
-								 {"to": u"orm['auth.Permission']", "symmetrical": "False", "blank": "True"}),
+				{"to": u"orm['auth.Permission']", "symmetrical": "False", "blank": "True"}),
 			"username": ("django.db.models.fields.CharField", [], {"unique": "True", "max_length": "30"})
 		},
 		u"contenttypes.contenttype": {
 			"Meta": {"ordering": "('name',)", "unique_together": "(('app_label', 'model'),)", "object_name": "ContentType",
-					 "db_table": "'django_content_type'"},
+				"db_table": "'django_content_type'"},
 			"app_label": ("django.db.models.fields.CharField", [], {"max_length": "100"}),
-			u"id": ("django.db.models.fields.AutoField", [], {"primary_key": "True"}),
+			"id": ("django.db.models.fields.AutoField", [], {"primary_key": "True"}),
 			"model": ("django.db.models.fields.CharField", [], {"max_length": "100"}),
 			"name": ("django.db.models.fields.CharField", [], {"max_length": "100"})
 		},
@@ -95,21 +95,21 @@ class Migration(SchemaMigration):
 			"Meta": {"object_name": "APNSDevice"},
 			"active": ("django.db.models.fields.BooleanField", [], {"default": "True"}),
 			"device_id": ("uuidfield.fields.UUIDField", [], {"max_length": "32", "null": "True", "blank": "True"}),
-			u"id": ("django.db.models.fields.AutoField", [], {"primary_key": "True"}),
+			"id": ("django.db.models.fields.AutoField", [], {"primary_key": "True"}),
 			"name": ("django.db.models.fields.CharField", [], {"max_length": "255", "null": "True", "blank": "True"}),
 			"registration_id": ("django.db.models.fields.CharField", [], {"unique": "True", "max_length": "64"}),
 			"user": ("django.db.models.fields.related.ForeignKey", [],
-					 {"to": u"orm['%s.%s']" % (User._meta.app_label, User._meta.object_name), "null": "True", "blank": "True"})
+				{"to": u"orm['%s.%s']" % (User._meta.app_label, User._meta.object_name), "null": "True", "blank": "True"})
 		},
 		u"push_notifications.gcmdevice": {
 			"Meta": {"object_name": "GCMDevice"},
 			"active": ("django.db.models.fields.BooleanField", [], {"default": "True"}),
 			"device_id": ("push_notifications.fields.HexIntegerField", [], {"null": "True", "blank": "True"}),
-			u"id": ("django.db.models.fields.AutoField", [], {"primary_key": "True"}),
+			"id": ("django.db.models.fields.AutoField", [], {"primary_key": "True"}),
 			"name": ("django.db.models.fields.CharField", [], {"max_length": "255", "null": "True", "blank": "True"}),
 			"registration_id": ("django.db.models.fields.TextField", [], {}),
 			"user": ("django.db.models.fields.related.ForeignKey", [],
-					 {"to": u"orm['%s.%s']" % (User._meta.app_label, User._meta.object_name), "null": "True", "blank": "True"})
+				{"to": u"orm['%s.%s']" % (User._meta.app_label, User._meta.object_name), "null": "True", "blank": "True"})
 		}
 	}
 
