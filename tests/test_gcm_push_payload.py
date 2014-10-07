0ccd3570d66bdad5f7a3c495725be0bef020c999
arthurprs <arthurprs@gmail.com>
2014-10-07 15:46:51 -0300

    decoupled _gcm_send error handling; adjusted tests accordingly

--- tests/test_gcm_push_payload.py
+++ tests/test_gcm_push_payload.py
@@ -2,25 +2,26 @@ import mock
 import json
 from django.test import TestCase
 from push_notifications.gcm import gcm_send_message, gcm_send_bulk_message
+from tests.mock_responses import GCM_PLAIN_RESPONSE, GCM_JSON_RESPONSE
 
 
 class GCMPushPayloadTest(TestCase):
 	def test_push_payload(self):
-		with mock.patch("push_notifications.gcm._gcm_send") as p:
+		with mock.patch("push_notifications.gcm._gcm_send", return_value=GCM_PLAIN_RESPONSE) as p:
 			gcm_send_message("abc", {"message": "Hello world"})
 			p.assert_called_once_with(
 				b"registration_id=abc&data.message=Hello+world",
 				"application/x-www-form-urlencoded;charset=UTF-8")
 
 	def test_push_payload_params(self):
-		with mock.patch("push_notifications.gcm._gcm_send") as p:
+		with mock.patch("push_notifications.gcm._gcm_send", return_value=GCM_PLAIN_RESPONSE) as p:
 			gcm_send_message("abc", {"message": "Hello world"}, delay_while_idle=True, time_to_live=3600)
 			p.assert_called_once_with(
 				b"delay_while_idle=1&registration_id=abc&time_to_live=3600&data.message=Hello+world",
 				"application/x-www-form-urlencoded;charset=UTF-8")
 
 	def test_push_nested_payload(self):
-		with mock.patch("push_notifications.gcm._gcm_send") as p:
+		with mock.patch("push_notifications.gcm._gcm_send", return_value=GCM_JSON_RESPONSE) as p:
 			payload = {
 				"message": "Hello world",
 				"extra": {
@@ -36,7 +37,7 @@ class GCMPushPayloadTest(TestCase):
 				"application/json")
 
 	def test_bulk_push_payload(self):
-		with mock.patch("push_notifications.gcm._gcm_send") as p:
+		with mock.patch("push_notifications.gcm._gcm_send", return_value=GCM_JSON_RESPONSE) as p:
 			gcm_send_bulk_message(["abc", "123"], {"message": "Hello world"})
 			p.assert_called_once_with(
 				b'{"data":{"message":"Hello world"},"registration_ids":["abc","123"]}',
