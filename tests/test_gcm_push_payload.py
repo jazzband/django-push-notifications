63a2bc175f8c9f3fc553627d57725b5408f4311d
Jerome Leclanche <jerome@leclan.ch>
2015-06-03 08:50:32 +0200

    Remove an inherently broken test
    See: https://stackoverflow.com/questions/11378004

--- tests/test_gcm_push_payload.py
+++ tests/test_gcm_push_payload.py
@@ -20,22 +20,6 @@ class GCMPushPayloadTest(TestCase):
 				b"data.message=Hello+world&delay_while_idle=1&registration_id=abc&time_to_live=3600",
 				"application/x-www-form-urlencoded;charset=UTF-8")
 
-	def test_push_nested_payload(self):
-		with mock.patch("push_notifications.gcm._gcm_send", return_value=GCM_JSON_RESPONSE) as p:
-			payload = {
-				"message": "Hello world",
-				"extra": {
-					"key0": ["value0_0", "value0_1", "value0_2"],
-					"key1": "value1",
-					"key2": {"key2_0": "value2_0"}
-				}
-			}
-			payload_string = json.dumps(payload, separators=(",", ":"), sort_keys=True).encode("utf-8")
-			gcm_send_message("abc", payload)
-			p.assert_called_once_with(
-				b'{"data":' + payload_string + b',"registration_ids":["abc"]}',
-				"application/json")
-
 	def test_bulk_push_payload(self):
 		with mock.patch("push_notifications.gcm._gcm_send", return_value=GCM_JSON_RESPONSE) as p:
 			gcm_send_bulk_message(["abc", "123"], {"message": "Hello world"})
