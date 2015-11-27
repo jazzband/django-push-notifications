4dc7c1e242397b840f714f3b24431d515eff173a
Vsevolod Novikov <nnseva@gmail.com>
2015-11-27 16:51:23 +0300

    Push notifications for multiple mobile applications - implementation

---
+++ push_notifications/dynamic.py
@@ -0,0 +1,48 @@
+from django.core.exceptions import ImproperlyConfigured
+from . import NotificationError
+from .settings import PUSH_NOTIFICATIONS_SETTINGS as SETTINGS
+from .modeldict import FieldPairDict
+
+def _get_application_settings(application_id,settings_key,error_message):
+    if not application_id: # old behaviour
+        value = SETTINGS.get(settings_key,None)
+        if not value:
+            raise ImproperlyConfigured(error_message)
+        return value
+    # new behaviour, settings dict
+    values = SETTINGS.get(settings_key+"S",{})
+
+    if values.has_key(application_id):
+        return values.get(application_id)
+
+    # new behaviour, getting from the model
+    values_model = SETTINGS.get(settings_key+"S_MODEL",None)
+    if values_model:
+        model = values_model.get('model',None)
+        key = values_model.get('key',None)
+        value = values_model.get('value',None)
+        if model and key and value:
+            values = FieldPairDict(model,key,value)
+
+    if values.has_key(application_id):
+        return values.get(application_id)
+
+    value = SETTINGS.get(settings_key,None)
+    if not value:
+        raise ImproperlyConfigured(error_message)
+    return value
+
+def get_gcm_api_key(application_id=None):
+    return _get_application_settings(application_id,"GCM_API_KEY",'You need to setup PUSH_NOTIFICATIONS_SETTINGS properly to send messages')
+
+def get_apns_certificate(application_id=None):
+    r = _get_application_settings(application_id,"APNS_CERTIFICATE",'You need to setup PUSH_NOTIFICATIONS_SETTINGS properly to send messages')
+    if not isinstance(r,basestring):
+        # probably the (Django) file, and file path should be got
+        if hasattr(r,'path'):
+            return r.path
+        elif hasattr(r,'has_key') and r.has_key('path'):
+            return r['path']
+        else:
+            raise ImproperlyConfigured("The APNS certificate settings value should be a string, or should have a 'path' attribute or key")
+    return r
---
+++ tests/test_dynamic_settings.py
@@ -0,0 +1,70 @@
+import json
+import mock
+from django.test import TestCase
+from django.utils import timezone
+from push_notifications import dynamic
+from django.conf import settings
+
+from django.core.exceptions import ImproperlyConfigured
+
+from push_notifications.models import ApplicationModel
+
+class DynamicSettingsTestCase(TestCase):
+    def test_base_settings(self):
+        settings.PUSH_NOTIFICATIONS_SETTINGS['TEST'] = 'test'
+        assert dynamic._get_application_settings('qwerty','TEST','Test Exception') == 'test'
+        settings.PUSH_NOTIFICATIONS_SETTINGS['TESTS'] = {
+            'qwerty':'uiop'
+        }
+        assert dynamic._get_application_settings('qwerty','TEST','Test Exception') == 'uiop'
+        assert dynamic._get_application_settings('uiop','TEST','Test Exception') == 'test'
+        ex = None
+        try:
+            dynamic._get_application_settings('qwerty','NOTPRESENT','Test Exception')
+        except Exception,ex:
+            pass
+        assert type(ex) == ImproperlyConfigured
+        assert ex.message == 'Test Exception'
+
+    def test_database_settings(self):
+        settings.PUSH_NOTIFICATIONS_SETTINGS['TEST'] = 'test'
+        settings.PUSH_NOTIFICATIONS_SETTINGS['TESTS_MODEL'] = {
+            'model':'push_notifications.ApplicationModel',
+            'key':'application_id',
+            'value':'gcm_api_key',
+        }
+        ApplicationModel.objects.create(application_id='qwerty2',gcm_api_key='uiop2')
+        assert dynamic._get_application_settings('qwerty2','TEST','Test Exception') == 'uiop2'
+        assert dynamic._get_application_settings('uiop2','TEST','Test Exception') == 'test'
+        ex = None
+        try:
+            dynamic._get_application_settings('qwerty','NOTPRESENT','Test Exception')
+        except Exception,ex:
+            pass
+        assert type(ex) == ImproperlyConfigured
+        assert ex.message == 'Test Exception'
+
+    def test_push_settings(self):
+        ex = None
+        try:
+            r = dynamic.get_gcm_api_key()
+        except Exception,ex:
+            pass
+        assert type(ex) == ImproperlyConfigured
+        settings.PUSH_NOTIFICATIONS_SETTINGS['GCM_API_KEY'] = 'testkey'
+        settings.PUSH_NOTIFICATIONS_SETTINGS['APNS_CERTIFICATE'] = 'testcert'
+        assert dynamic.get_gcm_api_key() == 'testkey'
+        assert dynamic.get_apns_certificate() == 'testcert'
+
+        settings.PUSH_NOTIFICATIONS_SETTINGS['GCM_API_KEYS'] = {
+            'qwerty':'uiopkey'
+        }
+        settings.PUSH_NOTIFICATIONS_SETTINGS['APNS_CERTIFICATES'] = {
+            'qwerty':'uiopcert'
+        }
+        assert dynamic.get_gcm_api_key() == 'testkey'
+        assert dynamic.get_apns_certificate() == 'testcert'
+        assert dynamic.get_gcm_api_key('uiop') == 'testkey'
+        assert dynamic.get_apns_certificate('uiop') == 'testcert'
+        assert dynamic.get_gcm_api_key('qwerty') == 'uiopkey'
+        assert dynamic.get_apns_certificate('qwerty') == 'uiopcert'
