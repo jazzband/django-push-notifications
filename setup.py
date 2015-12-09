8b0a9792b014428939b16103c615e778f5ba7d91
jamaalscarlett <jamaal.scarlett@gmail.com>
2015-12-09 04:17:18 +0000

    Dropped support for Python 3.3
    * Added Django 1.9 to tox test environment
    * Fixed tox.ini to properly test against all supported
      versions of Python, Django and Django Rest Framework

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ CLASSIFIERS = [
 	"Programming Language :: Python",
 	"Programming Language :: Python :: 2.7",
 	"Programming Language :: Python :: 3",
-	"Programming Language :: Python :: 3.3",
 	"Programming Language :: Python :: 3.4",
+	"Programming Language :: Python :: 3.5",
 	"Topic :: Software Development :: Libraries :: Python Modules",
 	"Topic :: System :: Networking",
 ]
