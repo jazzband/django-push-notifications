7fbaafde7d08b7126115a681f83942dc77aa2828
Vsevolod Novikov <nnseva@gmail.com>
2015-12-18 15:55:46 +0300

    Fixed Django 1.7 compatibility

--- push_notifications/models.py
+++ push_notifications/models.py
@@ -7,6 +7,11 @@ from django.utils.translation import ugettext_lazy as _
 
 from .fields import HexIntegerField
 
+try:
+	from django.db.models import UUIDField
+except:
+	from uuidfield import UUIDField
+
 
 @python_2_unicode_compatible
 class Device(models.Model):
@@ -78,7 +83,7 @@ class APNSDeviceQuerySet(models.query.QuerySet):
 
 
 class APNSDevice(Device):
-	device_id = models.UUIDField(verbose_name=_("Device ID"), blank=True, null=True, db_index=True,
+	device_id = UUIDField(verbose_name=_("Device ID"), blank=True, null=True, db_index=True,
 		help_text="UDID / UIDevice.identifierForVendor()")
 	registration_id = models.CharField(verbose_name=_("Registration ID"), max_length=64, unique=True)
 
