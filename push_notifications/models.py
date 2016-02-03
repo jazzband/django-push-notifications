4208fecb1a80d1048047fbd5b211491ea1a96a14
Fredrik Stockman <fredrik@threadsafestudio.com>
2016-02-03 01:07:29 +0100

    Enabled support for arbitrary user model

--- push_notifications/models.py
+++ push_notifications/models.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
 
-from django.conf import settings
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 from .fields import HexIntegerField
+from .settings import PUSH_NOTIFICATIONS_SETTINGS as SETTINGS
 
 
 @python_2_unicode_compatible
@@ -13,7 +13,7 @@ class Device(models.Model):
 	name = models.CharField(max_length=255, verbose_name=_("Name"), blank=True, null=True)
 	active = models.BooleanField(verbose_name=_("Is active"), default=True,
 		help_text=_("Inactive devices will not be sent notifications"))
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+	user = models.ForeignKey(SETTINGS["USER_MODEL"], blank=True, null=True)
 	date_created = models.DateTimeField(verbose_name=_("Creation date"), auto_now_add=True, null=True)
 
 	class Meta:
