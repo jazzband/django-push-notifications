6621cb5111d5fc89e1b10d50bb91632e0cdfc56d
Sylwester Brzeczkowski <sbrzeczkowski@mirantis.com>
2016-02-25 08:48:20 +0100

    #290 do not try to send any messages if registration_ids is empty

--- push_notifications/models.py
+++ push_notifications/models.py
@@ -10,91 +10,93 @@ from .settings import PUSH_NOTIFICATIONS_SETTINGS as SETTINGS
 
 @python_2_unicode_compatible
 class Device(models.Model):
-	name = models.CharField(max_length=255, verbose_name=_("Name"), blank=True, null=True)
-	active = models.BooleanField(verbose_name=_("Is active"), default=True,
-		help_text=_("Inactive devices will not be sent notifications"))
-	user = models.ForeignKey(SETTINGS["USER_MODEL"], blank=True, null=True)
-	date_created = models.DateTimeField(verbose_name=_("Creation date"), auto_now_add=True, null=True)
+    name = models.CharField(max_length=255, verbose_name=_("Name"), blank=True, null=True)
+    active = models.BooleanField(verbose_name=_("Is active"), default=True,
+        help_text=_("Inactive devices will not be sent notifications"))
+    user = models.ForeignKey(SETTINGS["USER_MODEL"], blank=True, null=True)
+    date_created = models.DateTimeField(verbose_name=_("Creation date"), auto_now_add=True, null=True)
 
-	class Meta:
-		abstract = True
+    class Meta:
+        abstract = True
 
-	def __str__(self):
-		return self.name or \
-			str(self.device_id or "") or \
-			"%s for %s" % (self.__class__.__name__, self.user or "unknown user")
+    def __str__(self):
+        return self.name or \
+            str(self.device_id or "") or \
+            "%s for %s" % (self.__class__.__name__, self.user or "unknown user")
 
 
 class GCMDeviceManager(models.Manager):
-	def get_queryset(self):
-		return GCMDeviceQuerySet(self.model)
+    def get_queryset(self):
+        return GCMDeviceQuerySet(self.model)
 
 
 class GCMDeviceQuerySet(models.query.QuerySet):
-	def send_message(self, message, **kwargs):
-		if self:
-			from .gcm import gcm_send_bulk_message
+    def send_message(self, message, **kwargs):
+        if self:
+            from .gcm import gcm_send_bulk_message
 
-			data = kwargs.pop("extra", {})
-			if message is not None:
-				data["message"] = message
+            data = kwargs.pop("extra", {})
+            if message is not None:
+                data["message"] = message
 
-			reg_ids = list(self.filter(active=True).values_list('registration_id', flat=True))
-			return gcm_send_bulk_message(registration_ids=reg_ids, data=data, **kwargs)
+            reg_ids = list(self.filter(active=True).values_list('registration_id', flat=True))
+            if not reg_ids:
+                return
+            return gcm_send_bulk_message(registration_ids=reg_ids, data=data, **kwargs)
 
 
 class GCMDevice(Device):
-	# device_id cannot be a reliable primary key as fragmentation between different devices
-	# can make it turn out to be null and such:
-	# http://android-developers.blogspot.co.uk/2011/03/identifying-app-installations.html
-	device_id = HexIntegerField(verbose_name=_("Device ID"), blank=True, null=True, db_index=True,
-		help_text=_("ANDROID_ID / TelephonyManager.getDeviceId() (always as hex)"))
-	registration_id = models.TextField(verbose_name=_("Registration ID"))
+    # device_id cannot be a reliable primary key as fragmentation between different devices
+    # can make it turn out to be null and such:
+    # http://android-developers.blogspot.co.uk/2011/03/identifying-app-installations.html
+    device_id = HexIntegerField(verbose_name=_("Device ID"), blank=True, null=True, db_index=True,
+        help_text=_("ANDROID_ID / TelephonyManager.getDeviceId() (always as hex)"))
+    registration_id = models.TextField(verbose_name=_("Registration ID"))
 
-	objects = GCMDeviceManager()
+    objects = GCMDeviceManager()
 
-	class Meta:
-		verbose_name = _("GCM device")
+    class Meta:
+        verbose_name = _("GCM device")
 
-	def send_message(self, message, **kwargs):
-		from .gcm import gcm_send_message
-		data = kwargs.pop("extra", {})
-		if message is not None:
-			data["message"] = message
-		return gcm_send_message(registration_id=self.registration_id, data=data, **kwargs)
+    def send_message(self, message, **kwargs):
+        from .gcm import gcm_send_message
+        data = kwargs.pop("extra", {})
+        if message is not None:
+            data["message"] = message
+        return gcm_send_message(registration_id=self.registration_id, data=data, **kwargs)
 
 
 class APNSDeviceManager(models.Manager):
-	def get_queryset(self):
-		return APNSDeviceQuerySet(self.model)
+    def get_queryset(self):
+        return APNSDeviceQuerySet(self.model)
 
 
 class APNSDeviceQuerySet(models.query.QuerySet):
-	def send_message(self, message, **kwargs):
-		if self:
-			from .apns import apns_send_bulk_message
-			reg_ids = list(self.filter(active=True).values_list('registration_id', flat=True))
-			return apns_send_bulk_message(registration_ids=reg_ids, alert=message, **kwargs)
+    def send_message(self, message, **kwargs):
+        if self:
+            from .apns import apns_send_bulk_message
+            reg_ids = list(self.filter(active=True).values_list('registration_id', flat=True))
+            return apns_send_bulk_message(registration_ids=reg_ids, alert=message, **kwargs)
 
 
 class APNSDevice(Device):
-	device_id = models.UUIDField(verbose_name=_("Device ID"), blank=True, null=True, db_index=True,
-		help_text="UDID / UIDevice.identifierForVendor()")
-	registration_id = models.CharField(verbose_name=_("Registration ID"), max_length=200, unique=True)
+    device_id = models.UUIDField(verbose_name=_("Device ID"), blank=True, null=True, db_index=True,
+        help_text="UDID / UIDevice.identifierForVendor()")
+    registration_id = models.CharField(verbose_name=_("Registration ID"), max_length=200, unique=True)
 
-	objects = APNSDeviceManager()
+    objects = APNSDeviceManager()
 
-	class Meta:
-		verbose_name = _("APNS device")
+    class Meta:
+        verbose_name = _("APNS device")
 
-	def send_message(self, message, **kwargs):
-		from .apns import apns_send_message
+    def send_message(self, message, **kwargs):
+        from .apns import apns_send_message
 
-		return apns_send_message(registration_id=self.registration_id, alert=message, **kwargs)
+        return apns_send_message(registration_id=self.registration_id, alert=message, **kwargs)
 
 
 # This is an APNS-only function right now, but maybe GCM will implement it
 # in the future.  But the definition of 'expired' may not be the same. Whatevs
 def get_expired_tokens():
-	from .apns import apns_fetch_inactive_ids
-	return apns_fetch_inactive_ids()
+    from .apns import apns_fetch_inactive_ids
+    return apns_fetch_inactive_ids()
