8737a4d7beda8f9a1655466a8aa88a44652c7971
Pablo Martín <goinnn@gmail.com>
2016-06-02 13:08:15 +0200

    APNS: Allow overriding the certfile from send_message()

--- push_notifications/apns.py
+++ push_notifications/apns.py
@@ -48,8 +48,8 @@ def _check_certificate(ss):
 		raise Exception("The certificate doesn't contain a private key")
 
 
-def _apns_create_socket(address_tuple):
-	certfile = SETTINGS.get("APNS_CERTIFICATE")
+def _apns_create_socket(address_tuple, certfile=None):
+	certfile = certfile or SETTINGS.get("APNS_CERTIFICATE")
 	if not certfile:
 		raise ImproperlyConfigured(
 			'You need to set PUSH_NOTIFICATIONS_SETTINGS["APNS_CERTIFICATE"] to send messages through APNS.'
@@ -75,12 +75,12 @@ def _apns_create_socket(address_tuple):
 	return sock
 
 
-def _apns_create_socket_to_push():
-	return _apns_create_socket((SETTINGS["APNS_HOST"], SETTINGS["APNS_PORT"]))
+def _apns_create_socket_to_push(certfile=None):
+	return _apns_create_socket((SETTINGS["APNS_HOST"], SETTINGS["APNS_PORT"]), certfile)
 
 
-def _apns_create_socket_to_feedback():
-	return _apns_create_socket((SETTINGS["APNS_FEEDBACK_HOST"], SETTINGS["APNS_FEEDBACK_PORT"]))
+def _apns_create_socket_to_feedback(certfile=None):
+	return _apns_create_socket((SETTINGS["APNS_FEEDBACK_HOST"], SETTINGS["APNS_FEEDBACK_PORT"]), certfile)
 
 
 def _apns_pack_frame(token_hex, payload, identifier, expiration, priority):
@@ -125,7 +125,7 @@ def _apns_check_errors(sock):
 
 def _apns_send(token, alert, badge=None, sound=None, category=None, content_available=False,
 	action_loc_key=None, loc_key=None, loc_args=[], extra={}, identifier=0,
-	expiration=None, priority=10, socket=None):
+	expiration=None, priority=10, socket=None, certfile=None):
 	data = {}
 	aps_data = {}
 
@@ -171,7 +171,7 @@ def _apns_send(token, alert, badge=None, sound=None, category=None, content_avai
 	if socket:
 		socket.write(frame)
 	else:
-		with closing(_apns_create_socket_to_push()) as socket:
+		with closing(_apns_create_socket_to_push(certfile)) as socket:
 			socket.write(frame)
 			_apns_check_errors(socket)
 
@@ -239,18 +239,19 @@ def apns_send_bulk_message(registration_ids, alert, **kwargs):
 	it won't be included in the notification. You will need to pass None
 	to this for silent notifications.
 	"""
-	with closing(_apns_create_socket_to_push()) as socket:
+	certfile = kwargs.get("certfile", None)
+	with closing(_apns_create_socket_to_push(certfile)) as socket:
 		for identifier, registration_id in enumerate(registration_ids):
 			_apns_send(registration_id, alert, identifier=identifier, socket=socket, **kwargs)
 		_apns_check_errors(socket)
 
 
-def apns_fetch_inactive_ids():
+def apns_fetch_inactive_ids(certfile=None):
 	"""
 	Queries the APNS server for id's that are no longer active since
 	the last fetch
 	"""
-	with closing(_apns_create_socket_to_feedback()) as socket:
+	with closing(_apns_create_socket_to_feedback(certfile)) as socket:
 		inactive_ids = []
 		# Maybe we should have a flag to return the timestamp?
 		# It doesn't seem that useful right now, though.
