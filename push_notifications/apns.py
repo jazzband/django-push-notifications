5cb0d53938ba53625f22845967d041b2e8297ddb
Jerome Leclanche <jerome@leclan.ch>
2016-12-15 19:39:18 +0200

    apns: Check for non-RSA formatted private keys
    Fixes #333
    Closes #340
    
    Thanks @piotrjakimiak

--- push_notifications/apns.py
+++ push_notifications/apns.py
@@ -51,10 +51,10 @@ def _check_certificate(ss):
 	mode = "start"
 	for s in ss.split("\n"):
 		if mode == "start":
-			if "BEGIN RSA PRIVATE KEY" in s:
+			if "BEGIN RSA PRIVATE KEY" in s or "BEGIN PRIVATE KEY" in s:
 				mode = "key"
 		elif mode == "key":
-			if "END RSA PRIVATE KEY" in s:
+			if "END RSA PRIVATE KEY" in s or "END PRIVATE KEY" in s:
 				mode = "end"
 				break
 			elif s.startswith("Proc-Type") and "ENCRYPTED" in s:
