8ad8cb49f7ab02dc2db76556f8bfe702ee565791
Jerome Leclanche <jerome@leclan.ch>
2017-03-08 13:11:32 +0200

    setup: Avoid importing the push_notifications module

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os.path
-import push_notifications
 from setuptools import setup
+
 
 README = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
 
@@ -29,12 +29,12 @@ setup(
 		"push_notifications/management",
 		"push_notifications/management/commands",
 	],
-	author=push_notifications.__author__,
-	author_email=push_notifications.__email__,
+	author="Jerome Leclanche",
+	author_email="jerome@leclan.ch",
 	classifiers=CLASSIFIERS,
 	description="Send push notifications to mobile devices through GCM or APNS in Django.",
 	download_url="https://github.com/jleclanche/django-push-notifications/tarball/master",
 	long_description=README,
 	url="https://github.com/jleclanche/django-push-notifications",
-	version=push_notifications.__version__,
+	version="1.4.1",
 )
