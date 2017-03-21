6310ba0731a93132907492196d980474e5259a7f
Jerome Leclanche <jerome@leclan.ch>
2017-03-21 02:18:08 +0200

    Clean up quote style, use flake8-quotes

--- push_notifications/management/commands/prune_devices.py
+++ push_notifications/management/commands/prune_devices.py
@@ -5,13 +5,13 @@ from push_notifications.utils import get_expired_tokens
 
 class Command(BaseCommand):
 	can_import_settings = True
-	help = 'Deactivate APNS devices that are not receiving notifications'
+	help = "Deactivate APNS devices that are not receiving notifications"
 
 	def handle(self, *args, **options):
 		expired = get_expired_tokens()
 		devices = APNSDevice.objects.filter(registration_id__in=expired)
 		for d in devices:
-			self.stdout.write('deactivating [%s]' % d.registration_id)
+			self.stdout.write("deactivating [%s]" % (d.registration_id))
 			d.active = False
 			d.save()
-		self.stdout.write('deactivated %d devices' % len(devices))
+		self.stdout.write("deactivated %d devices" % (len(devices)))
--- tests/test_management_commands.py
+++ tests/test_management_commands.py
@@ -9,15 +9,15 @@ class CommandsTestCase(TestCase):
 		from push_notifications.models import APNSDevice
 
 		device = APNSDevice.objects.create(
-			registration_id="616263",  # hex encoding of b'abc'
+			registration_id="616263",  # hex encoding of b"abc"
 		)
 
 		feedback_method = "push_notifications.apns._apns_create_socket_to_feedback"
 		recv_feedback_method = "push_notifications.apns._apns_receive_feedback"
 		with mock.patch(feedback_method, mock.MagicMock()):
 			with mock.patch(recv_feedback_method, mock.MagicMock()) as receiver:
-				receiver.side_effect = lambda s: [(b'', b'abc')]
-				call_command('prune_devices')
+				receiver.side_effect = lambda s: [(b"", b"abc")]
+				call_command("prune_devices")
 
 		device = APNSDevice.objects.get(pk=device.pk)
 		self.assertFalse(device.active)
