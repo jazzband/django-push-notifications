8bde76db966d080b20e2adbe14fd49c50cb0d5de
Daniel Hahler <git@thequod.de>
2017-05-17 13:14:49 +0200

    Remove push_notifications.utils
    It only contains `get_expired_tokens`, which is not importable anymore,
    since apns_fetch_inactive_ids has been removed in 3998996.
    
    Ref: https://github.com/jleclanche/django-push-notifications/issues/389

--- push_notifications/utils.py
+++
@@ -1,7 +0,0 @@
-from .apns import apns_fetch_inactive_ids
-
-
-# This is an APNS-only function right now, but maybe GCM will implement it
-# in the future.  But the definition of 'expired' may not be the same.
-def get_expired_tokens(application_id):
-	return apns_fetch_inactive_ids(application_id)
