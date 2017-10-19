d3e096ddfae7a823c4df34910369f90a1ddd33ca
Pablo Martín <goinnn@gmail.com>
2017-10-19 11:05:22 +0200

    Opera in help text

--- push_notifications/models.py
+++ push_notifications/models.py
@@ -233,7 +233,7 @@ class WebPushDevice(Device):
 	browser = models.CharField(
 		verbose_name=_("Browser"), max_length=10,
 		choices=BROWSER_TYPES, default=BROWSER_TYPES[0][0],
-		help_text=_("Currently only support to Chrome and Firefox browsers")
+		help_text=_("Currently only support to Chrome, Firefox and Opera browsers")
 	)
 	objects = WebPushDeviceManager()
 
