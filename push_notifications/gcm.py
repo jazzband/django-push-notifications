0615b316868d59e56cefc0db295aa273d8f27001
Nahim Nasser <nnasser@gmail.com>
2019-10-15 09:42:50 -0400

    Add additional error type of MismatchSenderId

--- push_notifications/gcm.py
+++ push_notifications/gcm.py
@@ -82,7 +82,7 @@ def _cm_handle_response(registration_ids, response_data, cloud_type, application
 				# If error is NotRegistered or InvalidRegistration, then we will deactivate devices
 				# because this registration ID is no more valid and can't be used to send messages,
 				# otherwise raise error
-				if error in ("NotRegistered", "InvalidRegistration"):
+				if error in ("NotRegistered", "InvalidRegistration", "MismatchSenderId"):
 					ids_to_remove.append(registration_ids[index])
 				else:
 					throw_error = True
