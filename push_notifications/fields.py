f0d6b792a3b7ed8ab53fe69091a06f8ff843858e
Esau Rodriguez <esau@playwager.co.uk>
2019-11-19 11:38:22 +0000

    Make unused arguments part of an asterisk parameter (#536)

--- push_notifications/fields.py
+++ push_notifications/fields.py
@@ -98,7 +98,7 @@ class HexIntegerField(models.BigIntegerField):
 			value = _unsigned_to_signed_integer(value)
 		return value
 
-	def from_db_value(self, value, expression, connection, context):
+	def from_db_value(self, value, *args):
 		""" Return an unsigned int representation from all db backends """
 		if value is None:
 			return value
