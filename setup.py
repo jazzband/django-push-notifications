75c77d9f5cbbe6f35f8cc1e587ce0af267d49206
sudobangbang <greg.corradini@cbre.com>
2019-11-23 09:04:49 -0800

    fix setup.py with version and find_packages

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
-    name="django-push-notifications"
+    name="django-push-notifications",
+	version="2.0.0.1",
+	packages=find_packages(),
 )
+
