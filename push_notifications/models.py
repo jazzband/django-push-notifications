c317e383bd8e6f3e8517eddb9b49276e12dc39b1
Jason Held <jasonsheld@gmail.com>
2020-07-24 12:52:36 -0400

    switch to gettext

--- push_notifications/models.py
+++ push_notifications/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .fields import HexIntegerField
 from .settings import PUSH_NOTIFICATIONS_SETTINGS as SETTINGS
