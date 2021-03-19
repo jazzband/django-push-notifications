fa03f03c5180519a8bbb08310f6cb86aad032d2d
Jannis Leidel <jannis@leidel.info>
2021-03-19 22:13:43 +0100

    Migrate to GitHub Actions.

--- push_notifications/__init__.py
+++ push_notifications/__init__.py
@@ -1,4 +1,7 @@
-import pkg_resources
+from pkg_resources import get_distribution, DistributionNotFound
 
-
-__version__ = pkg_resources.require("django-push-notifications")[0].version
+try:
+    __version__ = get_distribution("django-push-notifications").version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = None
