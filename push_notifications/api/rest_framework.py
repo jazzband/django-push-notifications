10639a2d6e28b85acf1bff9f52958e97baf313b9
Ian Later <later@usc.edu>
2023-08-18 20:21:38 -0700

    Allow APNS tokens of variable length. (#678)
    Co-authored-by: Ian Later <ilater@outlook.com>

--- push_notifications/api/rest_framework.py
+++ push_notifications/api/rest_framework.py
@@ -46,10 +46,10 @@ class APNSDeviceSerializer(ModelSerializer):
 		model = APNSDevice
 
 	def validate_registration_id(self, value):
-		# iOS device tokens are 256-bit hexadecimal (64 characters). In 2016 Apple is increasing
-		# iOS device tokens to 100 bytes hexadecimal (200 characters).
 
-		if hex_re.match(value) is None or len(value) not in (64, 200):
+		# https://developer.apple.com/documentation/uikit/uiapplicationdelegate/1622958-application
+		# As of 02/2023 APNS tokens (registration_id) "are of variable length. Do not hard-code their size."
+		if hex_re.match(value) is None:
 			raise ValidationError("Registration ID (device token) is invalid")
 
 		return value
