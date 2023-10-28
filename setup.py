a021a329d669de0c57c2e8208608c2ca28aa5ccf
Jamaal Scarlett <jamaal.scarlett@gmail.com>
2023-10-28 18:00:52 -0400

    Set correct content type for the long_description in setup.py (#694)

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.rst").read_text()
 setup(
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type='text/x-rst',
     use_scm_version={"version_scheme": "post-release"}
 )
