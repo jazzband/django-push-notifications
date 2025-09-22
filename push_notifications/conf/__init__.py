4dbd89b8925ac49473a2674a818f13046c826c37
miky-rola <mikyrola8@gmail.com>
2025-09-22 19:24:43 +0000

    Set the manageType to use union of the config

--- push_notifications/conf/__init__.py
+++ push_notifications/conf/__init__.py
@@ -1,22 +1,23 @@
 from django.utils.module_loading import import_string
+from push_notifications.settings import PUSH_NOTIFICATIONS_SETTINGS as SETTINGS
+from typing import Union
+from .app import AppConfig
+from .appmodel import AppModelConfig
+from .legacy import LegacyConfig
 
-from ..settings import PUSH_NOTIFICATIONS_SETTINGS as SETTINGS  # noqa: I001
-from .app import AppConfig  # noqa: F401
-from .appmodel import AppModelConfig  # noqa: F401
-from .legacy import LegacyConfig  # noqa: F401
+# ManagerType is an alias for the possible configuration manager classes
+# that can be loaded dynamically via SETTINGS["CONFIG"].
+ManagerType = Union[AppConfig, AppModelConfig, LegacyConfig]
+
+manager: ManagerType | None = None
 
 
-manager = None
-
-
-def get_manager(reload=False):
+def get_manager(reload: bool = False) -> ManagerType:
 	global manager
-
-	if not manager or reload is True:
+	if not manager or reload:
 		manager = import_string(SETTINGS["CONFIG"])()
-
 	return manager
 
 
-# implementing get_manager as a function allows tests to reload settings
+# implementing get_manager as a function allows tests to reload settings get_manager()
 get_manager()
