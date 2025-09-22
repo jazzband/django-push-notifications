4c6a244abc961de7204e31a6b5d61e306e2a34b9
miky-rola <mikyrola8@gmail.com>
2025-09-22 19:23:05 +0000

    Use right import path and refactor types

--- push_notifications/api/rest_framework.py
+++ push_notifications/api/rest_framework.py
@@ -4,9 +4,10 @@ from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer, Serializer, ValidationError
 from rest_framework.viewsets import ModelViewSet
 
-from ..fields import UNSIGNED_64BIT_INT_MAX_VALUE, hex_re
-from ..models import APNSDevice, GCMDevice, WebPushDevice, WNSDevice
-from ..settings import PUSH_NOTIFICATIONS_SETTINGS as SETTINGS
+from push_notifications.fields import UNSIGNED_64BIT_INT_MAX_VALUE, hex_re
+from push_notifications.models import APNSDevice, GCMDevice, WebPushDevice, WNSDevice
+from push_notifications.settings import PUSH_NOTIFICATIONS_SETTINGS as SETTINGS
+from typing import Any, Union, Dict, Optional
 
 
 # Fields
@@ -15,16 +16,16 @@ class HexIntegerField(IntegerField):
 	Store an integer represented as a hex string of form "0x01".
 	"""
 
-	def to_internal_value(self, data):
+	def to_internal_value(self, data: Union[str, int]) -> int:
 		# validate hex string and convert it to the unsigned
 		# integer representation for internal use
 		try:
-			data = int(data, 16) if type(data) != int else data
+			data = int(data, 16) if not isinstance(data, int) else data
 		except ValueError:
 			raise ValidationError("Device ID is not a valid hex number")
 		return super().to_internal_value(data)
 
-	def to_representation(self, value):
+	def to_representation(self, value: int) -> int:
 		return value
 
 
@@ -32,8 +33,13 @@ class HexIntegerField(IntegerField):
 class DeviceSerializerMixin(ModelSerializer):
 	class Meta:
 		fields = (
-			"id", "name", "application_id", "registration_id", "device_id",
-			"active", "date_created"
+			"id",
+			"name",
+			"application_id",
+			"registration_id",
+			"device_id",
+			"active",
+			"date_created",
 		)
 		read_only_fields = ("date_created",)
 
@@ -45,8 +51,7 @@ class APNSDeviceSerializer(ModelSerializer):
 	class Meta(DeviceSerializerMixin.Meta):
 		model = APNSDevice
 
-	def validate_registration_id(self, value):
-
+	def validate_registration_id(self, value: str) -> str:
 		# https://developer.apple.com/documentation/uikit/uiapplicationdelegate/1622958-application
 		# As of 02/2023 APNS tokens (registration_id) "are of variable length. Do not hard-code their size."
 		if hex_re.match(value) is None:
@@ -56,10 +61,10 @@ class APNSDeviceSerializer(ModelSerializer):
 
 
 class UniqueRegistrationSerializerMixin(Serializer):
-	def validate(self, attrs):
-		devices = None
-		primary_key = None
-		request_method = None
+	def validate(self, attrs: Dict[str, Any]) -> Dict[str, Any]:
+		devices: Optional[Any] = None
+		primary_key: Optional[Any] = None
+		request_method: Optional[str] = None
 
 		if self.initial_data.get("registration_id", None):
 			if self.instance:
@@ -76,9 +81,10 @@ class UniqueRegistrationSerializerMixin(Serializer):
 
 		Device = self.Meta.model
 		if request_method == "update":
-			reg_id = attrs.get("registration_id", self.instance.registration_id)
-			devices = Device.objects.filter(registration_id=reg_id) \
-				.exclude(id=primary_key)
+			reg_id: str = attrs.get("registration_id", self.instance.registration_id)
+			devices = Device.objects.filter(registration_id=reg_id).exclude(
+				id=primary_key
+			)
 		elif request_method == "create":
 			devices = Device.objects.filter(registration_id=attrs["registration_id"])
 
@@ -92,20 +98,26 @@ class GCMDeviceSerializer(UniqueRegistrationSerializerMixin, ModelSerializer):
 		help_text="ANDROID_ID / TelephonyManager.getDeviceId() (e.g: 0x01)",
 		style={"input_type": "text"},
 		required=False,
-		allow_null=True
+		allow_null=True,
 	)
 
 	class Meta(DeviceSerializerMixin.Meta):
 		model = GCMDevice
 		fields = (
-			"id", "name", "registration_id", "device_id", "active", "date_created",
-			"cloud_message_type", "application_id",
+			"id",
+			"name",
+			"registration_id",
+			"device_id",
+			"active",
+			"date_created",
+			"cloud_message_type",
+			"application_id",
 		)
 		extra_kwargs = {"id": {"read_only": False, "required": False}}
 
-	def validate_device_id(self, value):
+	def validate_device_id(self, value: Optional[int] = None) -> Optional[int]:
 		# device ids are 64 bit unsigned values
-		if value > UNSIGNED_64BIT_INT_MAX_VALUE:
+		if value is not None and value > UNSIGNED_64BIT_INT_MAX_VALUE:
 			raise ValidationError("Device ID is out of range")
 		return value
 
@@ -119,26 +131,36 @@ class WebPushDeviceSerializer(UniqueRegistrationSerializerMixin, ModelSerializer
 	class Meta(DeviceSerializerMixin.Meta):
 		model = WebPushDevice
 		fields = (
-			"id", "name", "registration_id", "active", "date_created",
-			"p256dh", "auth", "browser", "application_id",
+			"id",
+			"name",
+			"registration_id",
+			"active",
+			"date_created",
+			"p256dh",
+			"auth",
+			"browser",
+			"application_id",
 		)
 
 
 # Permissions
 class IsOwner(permissions.BasePermission):
-	def has_object_permission(self, request, view, obj):
+	def has_object_permission(self, request: Any, view: Any, obj: Any) -> bool:
 		# must be the owner to view the object
 		return obj.user == request.user
 
 
 # Mixins
 class DeviceViewSetMixin:
-	lookup_field = "registration_id"
+	lookup_field: str = "registration_id"
 
-	def create(self, request, *args, **kwargs):
-		serializer = None
-		is_update = False
-		if SETTINGS.get("UPDATE_ON_DUPLICATE_REG_ID") and self.lookup_field in request.data:
+	def create(self, request: Any, *args: Any, **kwargs: Any) -> Response:
+		serializer: Optional[Any] = None
+		is_update: bool = False
+		if (
+			SETTINGS.get("UPDATE_ON_DUPLICATE_REG_ID")
+			and self.lookup_field in request.data
+		):
 			instance = self.queryset.model.objects.filter(
 				registration_id=request.data[self.lookup_field]
 			).first()
@@ -155,23 +177,25 @@ class DeviceViewSetMixin:
 		else:
 			self.perform_create(serializer)
 			headers = self.get_success_headers(serializer.data)
-			return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+			return Response(
+				serializer.data, status=status.HTTP_201_CREATED, headers=headers
+			)
 
-	def perform_create(self, serializer):
+	def perform_create(self, serializer: Serializer) -> Any:
 		if self.request.user.is_authenticated:
 			serializer.save(user=self.request.user)
 		return super().perform_create(serializer)
 
-	def perform_update(self, serializer):
+	def perform_update(self, serializer: Serializer) -> Any:
 		if self.request.user.is_authenticated:
 			serializer.save(user=self.request.user)
 		return super().perform_update(serializer)
 
 
 class AuthorizedMixin:
-	permission_classes = (permissions.IsAuthenticated, IsOwner)
+	permission_classes: tuple = (permissions.IsAuthenticated, IsOwner)
 
-	def get_queryset(self):
+	def get_queryset(self) -> Any:
 		# filter all devices to only those belonging to the current user
 		return self.queryset.filter(user=self.request.user)
 
@@ -207,7 +231,7 @@ class WNSDeviceAuthorizedViewSet(AuthorizedMixin, WNSDeviceViewSet):
 class WebPushDeviceViewSet(DeviceViewSetMixin, ModelViewSet):
 	queryset = WebPushDevice.objects.all()
 	serializer_class = WebPushDeviceSerializer
-	lookup_value_regex = '.+'
+	lookup_value_regex: str = ".+"
 
 
 class WebPushDeviceAuthorizedViewSet(AuthorizedMixin, WebPushDeviceViewSet):
