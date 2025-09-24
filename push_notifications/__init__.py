0be78765979be34911357390d5bec0c51b04622d
jamaalscarlett <jamaal.scarlett@gmail.com>
2025-09-24 00:12:18 -0500

    Adds a deprection warning for python <= 3.9
    Python 3.9 is EOL in 10/2025.

--- push_notifications/__init__.py
+++ push_notifications/__init__.py
@@ -1,8 +1,19 @@
+import sys
+import warnings
+
 try:
     # Python 3.8+
     import importlib.metadata as importlib_metadata
 except ImportError:
     # <Python 3.7 and lower
     import importlib_metadata
+
+if sys.version_info < (3, 10):
+    warnings.warn(
+        "Python 3.9 and earlier support is deprecated and will be removed in a future version. "
+        "Please upgrade to Python 3.10 or later.",
+        UserWarning,
+        stacklevel=2
+    )    
 
 __version__ = importlib_metadata.version("django-push-notifications")
